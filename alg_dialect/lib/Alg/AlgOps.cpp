a4e80a2d24557521b474145e523b019487126e83
stomfaig <stomfaig@gmail.com>
2025-11-22 16:30:59 +0000

    add group verification for add op

--- alg_dialect/lib/Alg/AlgOps.cpp
+++ alg_dialect/lib/Alg/AlgOps.cpp
@@ -1,9 +1,26 @@
 #include "Alg/AlgOps.h"
 #include "Alg/AlgTypes.h"
 #include "Alg/AlgDialect.h"
+#include "mlir/Support/LLVM.h"
+#include "llvm/Support/LogicalResult.h"
 
 using namespace mlir;
 using namespace mlir::alg;
 
 #define GET_OP_CLASSES
 #include "Alg/AlgOps.cpp.inc"
+
+
+llvm::LogicalResult AddOp::verify() {
+    auto elem1 = getElem1();
+    auto elem2 = getElem2();
+
+    auto elem1Type = mlir::dyn_cast<ElementType>(elem1.getType());
+    auto elem2Type = mlir::dyn_cast<ElementType>(elem2.getType());
+
+    if (elem1Type.getGroup() != elem2Type.getGroup()) {
+        return llvm::failure();
+    }
+
+    return llvm::success();
+}
